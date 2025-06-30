074d7a9d2a4e621f4913c295600939df4caaa2b3
MohGendy <midoomidoooo12@gmail.com>
2025-07-01 02:05:40 +0300

    mux4to1

--- mux.v
+++ mux.v
@@ -1,4 +1,4 @@
-module mux (
+module mux2to1 (
     input wire [31:0] A,
     input wire [31:0] B,
     input wire sel,
@@ -7,5 +7,20 @@ module mux (
 );
 
     assign out = sel ? B : A;
+    
+endmodule
+
+module mux4to1 (
+    input wire [31:0] A,
+    input wire [31:0] B,
+    input wire [31:0] C,
+    input wire [31:0] D,
+    input wire [1:0] sel,
+    output wire [31:0] out
+);
+
+    assign out = (sel == 2'b00) ? A :
+                 (sel == 2'b01) ? B :
+                 (sel == 2'b10) ? C : D;
     
 endmodule
