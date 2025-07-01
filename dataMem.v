fc3e8396d11832a68eaf834e8e99fd0afe79c2bb
MohGendy <midoomidoooo12@gmail.com>
2025-07-01 21:07:30 +0300

    dataMem

--- dataMem.v
+++ dataMem.v
@@ -1,1 +1,19 @@
+module dataMem (
+    input wire clk,
+    input wire WE,
+    input wire [31:0] A,
+    input wire [31:0] WD,
+    output reg [31:0] RD
+);
+    reg [31:0]mem[0:63]; // Memory array with 64 words
 
+    always @(*) begin
+        RD = mem[A[31:2]]; // Read data from memory
+    end
+
+    always @(posedge clk) begin
+        mem[A[31:2]] <= WD; // Write data to memory on clock edge
+    end
+
+    
+endmodule
