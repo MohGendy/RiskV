e649064902eea667796f3d2e04cc1147106ce091
MohGendy <midoomidoooo12@gmail.com>
2025-07-01 16:09:15 +0300

    instrMem

--- instMem.v
+++ instMem.v
@@ -2,6 +2,8 @@ module InstrMem (
     input wire [31:0] pc,
     output reg [31:0] Instr
 );
-    reg [31:0]mem[63:0];
-    assign Instr = mem[pc[31:2]]; // Assuming pc is word-aligned
+    reg [31:0]mem[0:63];
+    always @(pc) begin
+        Instr = mem[pc[31:2]]; // Assuming pc is word-aligned
+    end
 endmodule
